387a81e06814ab499718612c6379710d0e2261cf
Anant Kanchan <anant4265@gmail.com>
2021-08-26 14:40:06 +0530

    :lipstick: ui

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
 
 class Home extends StatefulWidget {
   @override
@@ -6,10 +7,46 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  TextEditingController tec = TextEditingController();
+  var encryptedText, plainText;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("AEScrypter")),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: TextField(
+                controller: tec,
+              ),
+            ),
+            Column(
+              children: [
+                Text("Plain Text",style: TextStyle(fontSize: 25,color: Colors.blue[400],fontWeight: FontWeight.w600),),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                child: Text(plainText == null ? "" : plainText),
+                )
+              ],
+            ),
+
+                Column(
+                  children: [
+                    Text("Encrypted Text ",style: TextStyle(fontSize: 25,color: Colors.blue[400],fontWeight: FontWeight.w600),),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(encryptedText == null ? "" : encryptedText is encrypt.Encrypted ? encryptedText.base64 : encryptedText),
+                    )
+                  ],
+            ),
+          ],
+        ),
+      ),
+
+
     );
   }
 }
